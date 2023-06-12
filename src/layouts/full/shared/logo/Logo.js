5b24e9bd6ac70cd61b11827ccc0d5c17118da240
Javascript <javascript@Javascripts-MacBook-Pro.local>
2023-06-12 08:48:47 +0100

    simple modifications

--- src/layouts/full/shared/logo/Logo.js
+++ src/layouts/full/shared/logo/Logo.js
@@ -1,20 +1,24 @@
-import { Link } from 'react-router-dom';
-import { ReactComponent as LogoDark } from 'src/assets/images/logos/dark-logo.svg';
-import { styled } from '@mui/material';
+import { Link } from "react-router-dom";
+import { ReactComponent as LogoDark } from "src/assets/images/logos/dark-logo.svg";
+import { styled, Typography } from "@mui/material";
 
-const LinkStyled = styled(Link)(() => ({
-  height: '70px',
-  width: '180px',
-  overflow: 'hidden',
-  display: 'block',
+const LinkStyled = styled("p")(() => ({
+  height: "70px",
+  width: "180px",
+  overflow: "hidden",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
 }));
 
 const Logo = () => {
   return (
-    <LinkStyled to="/">
-      <LogoDark height={70} />
+    <LinkStyled>
+      <Typography sx={{ fontSize: "2rem", fontWeight: "900" }}>
+        Balady .AI
+      </Typography>
     </LinkStyled>
-  )
+  );
 };
 
 export default Logo;
