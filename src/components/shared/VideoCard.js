39945008da0a3f1b46a7a8ed7b9fc555ed76080e
Yassine Farroud <yassinefarroud@protonmail.com>
2023-07-03 11:04:53 +0100

    mod

--- src/components/shared/VideoCard.js
+++ src/components/shared/VideoCard.js
@@ -1,9 +1,23 @@
-import ReactPlayer from 'react-player'
-import BlankCard from './BlankCard';
-import { Play } from "./Icons"
+import ReactPlayer from "react-player";
+import BlankCard from "./BlankCard";
+import { Play } from "./Icons";
 const VideoCard = ({ url }) => {
-    return <BlankCard><ReactPlayer playIcon={<Play />} url={url} width="100%" /></BlankCard>
-
-}
+  return (
+    <BlankCard>
+      {/* <ReactPlayer
+        playIcon={<Play />}
+        url={"http://localhost:3000/streams/output.m3u8"}
+        width="100%"
+        
+      /> */}
+      <iframe
+        style={{ minWidth: "100%", aspectRatio: 16 / 9 }}
+        frameborder="0"
+        src="http://localhost:3000/"
+        title="video-stream"
+      ></iframe>
+    </BlankCard>
+  );
+};
 
 export default VideoCard;
